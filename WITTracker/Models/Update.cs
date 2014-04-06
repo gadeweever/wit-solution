e340c81a49428e0b5bc3215fa981c8f83f5ecc1c
gadeweever <gamaldeweever@gmail.com>
2014-04-06 03:41:59 -0500

    Right before completely breaking it

---
+++ WITTracker/Models/Update.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WITTracker.Models
+{
+    public class Update
+    {
+
+        public int ID { get; set; }
+        public string Header { get; set; }
+        public string Text { get; set; }
+        public DateTime TimePosted { get; set; }
+        public int TeacherID { get; set; }
+
+        public virtual Teacher Author { get; set; }
+    }
+}
